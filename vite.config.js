55bee412f642f147b33600bfb8e60ec668d8a09c
KemuelJoshua <kemueljoshuamariano@gmail.com>
2025-03-02 11:37:51 +0000

    vite login config

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
                 'resources/sass/app.scss',
                 'resources/js/app.js',
  		'resources/sass/login.scss',
-		'resources/js/auth.js'
+		'resources/js/auth.js',
+		'resources/sass/dashboard.scss',
             ],
             refresh: true,
         }),
