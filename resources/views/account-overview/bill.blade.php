015ba7073684a3dc7b1c2433066c23961a1351a6
Novulutions-Jesner <jesner@novulutions.com>
2025-11-03 23:10:23 +0800

    Modified the other soa

--- resources/views/account-overview/bill.blade.php
+++ resources/views/account-overview/bill.blade.php
@@ -323,7 +323,7 @@
                                 <div style="display: flex; justify-content: space-between; margin: 5px 0 5px 0;">
                                     <div style="font-size: 20px; font-weight: 800; text-transform: uppercase">Current Billing:</div>
                                     <div style="font-size: 20px; font-weight: 800; text-transform: uppercase">
-                                        {{ number_format(abs((float) $data['current_bill']['total'] - (float) $arrears - (float) $totalDiscount - (float) ($franchise->amount ?? 0)), 2) }}
+                                        {{ number_format(abs((float) $data['current_bill']['amount'] - (float) $arrears - (float) $totalDiscount - (float) ($franchise->amount ?? 0)), 2) }}
                                     </div>
                                 </div>
                                 @if($arrears != 0)
