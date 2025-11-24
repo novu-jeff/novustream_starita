2fc809a226c5849b03c8a1e86c9a11e24f629cc0
Novulutions-Jesner <jesner@novulutions.com>
2025-11-24 13:46:04 +0800

    added dd

--- app/Services/MeterService.php
+++ app/Services/MeterService.php
@@ -636,7 +636,13 @@ class MeterService {
 
         $remainingUnpaid = max($unpaidAmount - $partialPaymentTotal, 0);
 
-
+        dd(
+    ["remaining" => $remainingUnpaid,
+    "amount" => $unpaidAmount,
+    "partial" => $partialPaymentTotal,
+    "bill_id" => $latestUnpaidBill?->id,
+    "reading_ids" => $readingIds,]
+);
         $other_deductions = $this->paymentBreakdownService::getData();
         $deductions = [
             [
